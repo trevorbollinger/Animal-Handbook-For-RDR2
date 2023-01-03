4255dbbc59ab4bf83fc418469fe5fa70078a14b1
trevorbollinger <68718850+trevorbollinger@users.noreply.github.com>
2023-01-02 18:14:05 -0600

    Initial Commit

---
+++ Hunting Handbook/Hunting_HandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hunting_HandbookApp.swift
+//  Hunting Handbook
+//
+//  Created by Trevor Bollinger on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Hunting_HandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
